2fc3d5856548c957763c5b35624379eba944107d
Asef00 <asefhosseini00@gmail.com>
2024-09-24 17:17:48 +0330

    configure prettier and format code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import logoSrc from "/money-bag.png"
+import logoSrc from '/money-bag.png'
 
 export default function Header() {
-    return (
-        <header className="flex items-center gap-4 p-5 flex-col">
-            <img src={logoSrc} alt="logo" />
-            <h1 className="text-3xl font-semibold">Investment Calculator</h1>
-        </header>
-    )
+  return (
+    <header className="flex items-center gap-4 p-5 flex-col">
+      <img src={logoSrc} alt="logo" />
+      <h1 className="text-3xl font-semibold">Investment Calculator</h1>
+    </header>
+  )
 }
--- src/utils/invetments.ts
+++ src/utils/invetments.ts
@@ -1,29 +1,29 @@
 export function calculateInvestmentResults({
-    initialInvestment,
-    annualInvestment,
-    expectedReturn,
-    duration
+  initialInvestment,
+  annualInvestment,
+  expectedReturn,
+  duration,
 }) {
-    const annulData = []
-    let investmentValue = initialInvestment
+  const annulData = []
+  let investmentValue = initialInvestment
 
-    for (let i = 0; i < duration; i++) {
-        const interestEarnedInYear = investmentValue * expectedReturn
-        investmentValue += interestEarnedInYear + annualInvestment
-        annulData.push({
-            year: i + 1,
-            interest: interestEarnedInYear,
-            valueEndOfYear: investmentValue,
-            annualInvestment:annualInvestment
-        })
-    }
+  for (let i = 0; i < duration; i++) {
+    const interestEarnedInYear = investmentValue * expectedReturn
+    investmentValue += interestEarnedInYear + annualInvestment
+    annulData.push({
+      year: i + 1,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: annualInvestment,
+    })
+  }
 
-    return annulData
+  return annulData
 }
 
 export const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
 })
