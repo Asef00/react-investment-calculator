baf5903c094a35fd922cf181e607369923da5a13
Asef00 <asefhosseini00@gmail.com>
2024-10-10 12:34:50 +0330

    fix: convert input values to number for correct calculations

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   function handleInputUpdate(inputValue: string, id: string) {
     setFormData((prevFormData) => {
-      return { ...prevFormData, [id]: inputValue }
+      return { ...prevFormData, [id]: +inputValue }
     })
   }
 
